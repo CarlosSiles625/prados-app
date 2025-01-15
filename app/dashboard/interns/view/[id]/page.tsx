13a2b6893c37d73374c29bbdb85b33486b4e38ce
CarlosSiles625 <saruman627@gmail.com>
2025-01-15 10:53:24 -0400

    refactor: replace Button component with styled Link for editing intern

--- app/dashboard/interns/view/[id]/page.tsx
+++ app/dashboard/interns/view/[id]/page.tsx
@@ -15,7 +15,6 @@ import { calculateAge } from "@/lib/utils";
 import { DischargeIntern } from "../../_components/interns/discharge-intern";
 import { DownIntern } from "../../_components/down-intern";
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
 import { getRecordByInternId } from "@/services/record";
 import { RecordsTable } from "../../_components/interns/record/record-table";
 import { Historial } from "@/types/record";
@@ -159,8 +158,11 @@ export default async function Page({
               </p>
             </div>
           )}
-          <Link href={`/dashboard/interns/edit/${id}`} passHref legacyBehavior>
-            <Button variant="secondary">Editar</Button>
+          <Link
+            href={`/dashboard/interns/edit/${id}`}
+            className="inline-flex items-center justify-center rounded-md bg-primary px-4 py-2 text-sm font-medium text-white shadow hover:bg-primary/90 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2"
+          >
+            Editar
           </Link>
         </section>
         <div>
