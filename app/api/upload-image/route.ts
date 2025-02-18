3b7ecf0f8cd59b9d7386271af191c9353b02e722
CarlosSiles625 <saruman627@gmail.com>
2025-02-18 12:08:39 -0400

    fix: disable no-explicit-any rule in ESLint configuration for upload-image route

--- app/api/upload-image/route.ts
+++ app/api/upload-image/route.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import path from "node:path";
 import { promises as fs } from "fs";
 import { getInternById, updateIntern } from "@/services/interns";
